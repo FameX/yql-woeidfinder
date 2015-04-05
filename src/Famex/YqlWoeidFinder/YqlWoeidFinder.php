bd0192e63d2d5f6016dc34e98824b5bc173ec426
Dominik Schwind <dschwind@lostfocus.de>
2015-04-05 23:33:59 +0200

    Using the LatLongDistanceHelper

--- src/Famex/YqlWoeidFinder/YqlWoeidFinder.php
+++ src/Famex/YqlWoeidFinder/YqlWoeidFinder.php
@@ -1,6 +1,8 @@
 <?php
 namespace Famex\YqlWoeidFinder;
 
+use Famex\Helpers\LatLongDistHelper;
+
 /**
  * Class YqlWoeidFinder
  * @package Famex\YqlWoeidFinder
@@ -219,18 +221,6 @@ class YqlWoeidFinder
 
     protected function _latLongDistance($lat1, $lon1, $lat2, $lon2, $unit = "K")
     {
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $miles = $dist * 60 * 1.1515;
-        $unit = strtoupper($unit);
-        if ($unit == "K") {
-            return ($miles * 1.609344);
-        } else if ($unit == "N") {
-            return ($miles * 0.8684);
-        } else {
-            return $miles;
-        }
+		return LatLongDistHelper::calculate($lat,$lon,$lat2,$lon2,$unit);
     }
 }
