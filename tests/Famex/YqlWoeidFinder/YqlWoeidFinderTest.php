80a6711d72f180dc0ea4c5b668abcfb6ff1b234d
Dominik Schwind <dschwind@lostfocus.de>
2015-04-05 21:11:06 +0200

    Test getPlaceFromWoeid

--- tests/Famex/YqlWoeidFinder/YqlWoeidFinderTest.php
+++ tests/Famex/YqlWoeidFinder/YqlWoeidFinderTest.php
@@ -23,4 +23,23 @@ class YqlWoeidFinderTest extends PHPUnit_Framework_TestCase {
 		}
 
 	}
+
+	public function testGetPlaceFromWoeid(){
+		try {
+			$place = $this->yqlWoeidFinder->getPlaceFromWoeid('28347326');
+			$this->assertInstanceOf('Famex\YqlWoeidFinder\Place',$place,"Did not return a place object");
+			$this->assertInstanceOf('Famex\YqlWoeidFinder\WoEID',$place->getWoeid(),"The place object does not have a woeid object");
+
+			$this->assertInstanceOf('Famex\YqlWoeidFinder\WoEID',$place->timezone,"The place object does not have a propoer timezone");
+			$this->assertEquals('Asia/Kuala_Lumpur',$place->timezone->content,"The place object has the wrong timezone");
+
+			$place = $this->yqlWoeidFinder->getPlaceFromWoeid('0');
+			$this->assertNull($place,"This returns something when it shouldn't");
+
+		} catch (Buzz\Exception\RequestException $e){
+			$this->markTestSkipped(
+				'Unable to connect to the YQL service.'
+			);
+		}
+	}
 }
