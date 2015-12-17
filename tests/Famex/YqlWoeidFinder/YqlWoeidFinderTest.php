7cc5f067856521c595fa87f609a91e45f1056eb4
Dominik Schwind <dschwind@lostfocus.de>
2015-12-17 11:58:52 +0100

    travis / composer / new test

--- tests/Famex/YqlWoeidFinder/YqlWoeidFinderTest.php
+++ tests/Famex/YqlWoeidFinder/YqlWoeidFinderTest.php
@@ -23,4 +23,20 @@ class YqlWoeidFinderTest extends PHPUnit_Framework_TestCase {
 		}
 
 	}
+	public function testGetSecondPlace(){
+		try {
+			$place = $this->yqlWoeidFinder->getPlace(53.5512,10);
+			$this->assertInstanceOf('Famex\YqlWoeidFinder\Place',$place,"Did not return a place object");
+			$this->assertInstanceOf('Famex\YqlWoeidFinder\WoEID',$place->getWoeid(),"The place object does not have a woeid object");
+			$this->assertEquals('12832451',$place->getWoeid()->woeid,'The place object has a wrong woeid for this lat/long');
+
+			$this->setExpectedException('Exception');
+			$place = $this->yqlWoeidFinder->getPlace(0,0);
+		} catch (Buzz\Exception\RequestException $e){
+			$this->markTestSkipped(
+				'Unable to connect to the YQL service.'
+			);
+		}
+
+	}
 }
